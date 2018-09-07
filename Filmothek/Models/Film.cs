b9ebd1c9de6081a50ba89c85b0189641eb77a598
NickRegnard <nikiliev23@gmail.com>
2018-09-07 14:58:52 +0200

    a

---
+++ Filmothek/Models/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filmothek.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string FilmName { get; set; }
+        public string Genre { get; set; }
+        public string Lenght { get; set; }
+        public bool IsSeries { get; set; }
+        public long Rating { get; set; }
+        public long Prise { get; set; }
+        public string Language { get; set; }
+        public string Release { get; set; }
+        public bool FSK { get; set; }
+        public string Inhalt { get; set; }
+    }
+}
